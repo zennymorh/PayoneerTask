a7c37b3d3a6972dcbec0f40a462613d72fa4af47
Zainab Jimoh <jimohzainab4753@gmail.com>
2021-12-31 16:49:48 +0100

    Refactoring Splashscreen for Android 12

--- app/src/main/java/com/zennymorh/payoneertask/ui/activity/MainActivity.java
+++ app/src/main/java/com/zennymorh/payoneertask/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zennymorh.payoneertask.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.zennymorh.payoneertask.R;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/zennymorh/payoneertask/ui/activity/SplashActivity.java
+++ app/src/main/java/com/zennymorh/payoneertask/ui/activity/SplashActivity.java
@@ -1,15 +1,16 @@
 package com.zennymorh.payoneertask.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        SplashScreen.installSplashScreen(this);
+//
+//    }
 }
